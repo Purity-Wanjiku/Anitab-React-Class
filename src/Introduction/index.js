f23ab7399b6ef667238b1c8a1bd3a8ff2d393cca
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-06-30 13:12:28 +0300

    Getting started with React framework for Js

---
+++ src/Introduction/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+import { FaReact } from 'react-icons/fa';
+
+    
+    
+const Introduction = () => {
+    return (
+        <div>
+          <h1 className="heading">What I have learnt in React</h1>
+            <p>React is JavaSCript library</p>
+            <FaReact size={30} />
+        </div>
+    )ls
+}
+
+export default Introduction;
